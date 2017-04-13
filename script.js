5442c287fe23e0f88c9e3ab3bad5e15f0a35c525
Jennifer Naasz <naasz.jl@gmail.com>
2017-04-13 14:04:17 -0400

    Upload Lab5

---
+++ script.js
@@ -0,0 +1,10 @@
+var farenheit = 98.6
+var farenheit2 = (farenheit - 32)
+var celcius = (farenheit2 * (5/9))
+
+console.log("Temperature is " + celcius + " Degrees Celcius");
+
+var celcius = 37;
+var farenheit = celcius / (5/9) + 32;
+
+console.log(farenheit + " Degrees farenheit")
