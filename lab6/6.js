23c0b51e9462b62874caf9b7f3441d57d54fe60b
Jennifer Naasz <naasz.jl@gmail.com>
2017-04-18 09:18:49 -0400

    Updated Lab6 to consol.log all arguments

--- lab6/6.js
+++ lab6/6.js
@@ -1,8 +1,17 @@
+//Creating a function to convert to Celcius from Farenheit or the reverse:
 function fToC(deg , Temp) {
   if(Temp === "F"){
     return(deg / (5/9) + 32);
   } else {
-    return((deg - 32) * (5/9));
+    console.log((deg - 32) * (5/9));
   }
 }
-console.log(fToC(100 , "F"));
+
+fToC(212, "C"); // 100
+fToC(32, "C"); // 0
+fToC(65, "C"); //18.333
+fToC(100, "F"); //212
+fToC(-40, "F"); //-40
+
+
+//Temp === F means we are solving for Farenheit
