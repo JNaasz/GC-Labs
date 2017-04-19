341202c8b350a891e84d1a891f12545f6d0fe9d5
Jennifer Naasz <naasz.jl@gmail.com>
2017-04-19 13:03:58 -0400

    added Lab8 to Lab7

--- lab7/script.js
+++ lab7/script.js
@@ -18,3 +18,21 @@ var total = 0
      });
       console.log("  ")
       console.log("Total: $" + total);
+
+var container = document.createElement('div');
+container.className = 'container';
+document.body.appendChild(container);
+
+var title = document.createElement('h1');
+title.innerText = 'Shopping Cart';
+container.appendChild(title);
+
+var list = document.createElement('ol');
+list.style.border = '2px solid black';
+container.appendChild(list);
+
+var house = "work"
+
+var listItem = document.createElement('li');
+listItem.innerText = cart[0].name, cart[0].price ;
+list.appendChild(listItem);
