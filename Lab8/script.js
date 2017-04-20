573248d494ee9b777e4e712d47589f2daa173bc8
Jennifer Naasz <naasz.jl@gmail.com>
2017-04-19 22:39:05 -0400

    adding bonus for lab8

--- Lab8/script.js
+++ Lab8/script.js
@@ -26,6 +26,7 @@ function listItems(){
    })
 }
 
+//TOTAL PAGE CONTAINER
 var container = document.createElement('div');
 container.className = 'container';
 container.style.position = 'absolute';
@@ -37,6 +38,7 @@ document.body.appendChild(container);
 //Title of Page
 var title = document.createElement('h1');
 title.innerText = 'Shopping Cart';
+title.style.fontFamily = 'cursive';
 title.style.color = 'indigo';
 title.style.margin = '10px';
 container.appendChild(title);
@@ -57,6 +59,7 @@ var spot = ''
   var listItem = document.createElement('li');
   listItem.innerText = i.name + ":           "  + " $" + i.price ;
   list.appendChild(listItem);
+  listItem.style.fontFamily = 'cursive';
   listItem.style.padding = '5px';
 })
 
@@ -70,5 +73,58 @@ totalPrice.style.padding = "10px";
 totalPrice.style.textAlign = 'center';
 totalPrice.style.margin = '10px';
 totalPrice.style.float = 'right';
+totalPrice.style.fontFamily = 'cursive';
 totalPrice.style.backgroundColor = 'mediumPurple';
 container.appendChild(totalPrice);
+
+//FORM FOR ADDING TO ARRAY
+var inputContainer = document.createElement('fieldset');
+inputContainer.style.fontFamily = 'cursive';
+inputContainer.style.marginLeft = '10px';
+inputContainer.style.marginTop = '10px';
+inputContainer.style.padding = '10px';
+inputContainer.style.border = '2px solid darkSlateBlue';
+container.appendChild(inputContainer);
+
+var legend = document.createElement('legend');
+legend.innerText = 'Add Item To List';
+legend.fontFamily = 'cursive';
+inputContainer.appendChild(legend);
+
+var itemForm = document.createElement('form');
+itemForm.onsubmit = 'addToCart()';
+inputContainer.appendChild(itemForm);
+
+//         itemLabel.htmlFor = 'inItem';
+var itemLabel = document.createElement('label');
+itemLabel.innerText = 'Item: ';
+itemForm.appendChild(itemLabel);
+
+var putInItem = document.createElement('input');
+putInItem.type = "text";
+putInItem.id = 'inItem';
+putInItem.style.width = "150px";
+itemForm.appendChild(putInItem);
+
+//        itemLabel.htmlFor = inPricetem;
+var itemLabel2 = document.createElement('label');
+itemLabel2.innerText = 'Price: ';
+inputContainer.appendChild(itemLabel2);
+
+var putInPrice = document.createElement('input');
+putInPrice.type = "text";
+putInPrice.id = 'inPrice';
+putInPrice.style.width = "150px";
+inputContainer.appendChild(putInPrice);
+
+//      SUBMIT BUTTON
+var click = document.createElement('button');
+click.innerText = 'Add';
+//click.onsubmit = 'addToCart()'
+inputContainer.appendChild(click);
+
+function addToCart(item , price1){
+  var newItemCombo = { name: item , price: price1};
+  cart.push(newItemCombo);
+  return cart;
+}
