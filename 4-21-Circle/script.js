aeaad3a7a74905ddb9dc5d9bd9e45abd80bdbe99
Jennifer Naasz <naasz.jl@gmail.com>
2017-04-24 00:28:53 -0400

    Finished Lab

--- 4-21-Circle/script.js
+++ 4-21-Circle/script.js
@@ -7,27 +7,30 @@ function area(){
  var area = pi * (r * r) ;
  var announce = document.getElementById('announce');
  announce.innerText = "You have created a circle with an area of " + area;
+ var happy = document.getElementById('happy');
+ happy.innerText = "Lets make every day EARTH DAY!";
 (function (){
   var circle = document.getElementById('circle');
   circle.style.backgroundColor = '#009947';
-  var diameter = r*4;
+  var diameter = r*3;
   circle.style.width = diameter + 'px';
   circle.style.height = diameter + 'px';
+  circle.style.backgroundSize = diameter + 'px';
   circle.style.borderRadius = '50%';
   }());
  };
 
  function changeColor(){
    console.log('mouse');
-   document.getElementById("circle").style.background = "radial-gradient(#009947, #b0a394, #ffccff)";
+   document.getElementById("circle").style.backgroundImage = "url('images/globe3.jpg')";
  };
 
  function changeColorBack(){
-   document.getElementById("circle").style.background = "radial-gradient(#ccccff, #b0a394, #009947)";
+   document.getElementById("circle").style.backgroundImage = "url('images/globe.jpg')";
  };
 
 
- 
+
 
 
 
